e2eab44f9f64bd9feecee610c0e483ca6c1eee38
cezar-codinarts <cezaraugusto@codinarts.com>
2020-11-12 16:56:41 -0400

    docs: command in package

--- exports.js
+++ exports.js
@@ -3,7 +3,8 @@ const { sort } = require("./service/sort_data")
 const readXlsxFile = require("read-excel-file/node")
 const { load, save, append, truncate } = require("./service/content_service")
 const fileResult = "./data_sql/file_result.sql"
-const pathXlsxFile = "./tmp/CRONOGRAMA_OS_SHOPPINGS_alterado_bia.xlsx"
+// const pathXlsxFile = "./tmp/CRONOGRAMA_OS_SHOPPINGS_alterado_bia.xlsx"
+// const pathXlsxFile = "./tmp/ordens_servico_datas.xlsx"
 const pathFile2Generate = "./content/file_2_generate.json"
 const testData2Generate = "./content/test_data_2_generate.json"
 
--- run.js
+++ run.js
@@ -5,7 +5,7 @@ const { pathXlsxFile, fileResult, pathFile2Generate, testData2Generate } = requi
 async function main () {
     try {
         await dataController.readXlsx(pathXlsxFile, testData2Generate)
-        await dataController.compare(testData2Generate, pathFile2Generate)
+        // await dataController.compare(testData2Generate, pathFile2Generate)
         await dataController.generate(testData2Generate, fileResult)
     } catch (err) {
         console.log(err)
